47f647e27f7a6cc3487e66806839be105203ccf7
AbdifataH-hersi <mohamedabdifatah107@gmail.com>
2024-09-14 01:36:20 +0900

    Exercise two Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,20 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+
+ let num1 = 200;
+ let num2 = 100;
+ let num3 = 50;
+
+  if (num1 > num2 && num1 > num3) {
+     console.log("The largest number out of the three numbers is " + num1 );
+
+  }else if (num2 > num1 && num2 > num3){
+    console.log("The largest number out of the three numbers is " + num2 );
+  }else if (num3 > num2 && num3 > num2) {
+
+ console.log("The largest number out of the three numbers is + " + num3);
+  }else {
+    console.log("Two or more numbers are equal and are the largest");
+  }
